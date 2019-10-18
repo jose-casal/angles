7b257c8e6f82a8f2ff7c612a4028f1fe2523affd
Jose Casal <jec85@cam.ac.uk>
2019-10-18 15:04:46 +0100

    New version of comets.R

--- comets.R
+++ comets.R
@@ -1,5 +1,7 @@
 comets <- read.csv("comets.csv")
 
+comets <- tbl_df(comets) %>% group_by(Track.ID) %>% mutate(mean_α= round(mean(α,  na.rm = TRUE),2))
+
 clean_comets <- bind_rows(tbl_df(comets) %>%
                             group_by(Track.ID) %>%
                             top_n(-1, Point.ID), tbl_df(comets) %>%
@@ -9,16 +11,22 @@ clean_comets <- bind_rows(tbl_df(comets) %>%
 clean_comets <- arrange(clean_comets,Track.ID)
 
 clean_comets <- clean_comets %>%
-  select(Track.ID, -Point.ID, x, y) %>%
-  gather(variable, value, -(Track.ID))%>%
+  select(Track.ID, mean_α, -Point.ID, x, y) %>%
+  gather(variable, value, -(mean_α), -(Track.ID))%>%
   mutate(Coo=rep(c("low", "high"), n()/2)) %>%
   unite(temp, variable, Coo) %>%
   group_by(Track.ID) %>%
   spread(temp, value) %>%
-  select(Track.ID, x_low, y_low, x_high, y_high) %>%
-  mutate(total_deg=round(ifelse((y_high-y_low)<0, rad2deg(atan((y_high-y_low)/(x_high-x_low)))-ifelse((x_high-x_low)<0, 180, 0),rad2deg(atan((y_high-y_low)/(x_high-x_low)))+ifelse((x_high-x_low)<0, 180, 0)),2))
+  select(Track.ID, x_low, y_low, x_high, y_high, mean_α) %>%
+  mutate(Angle=round(ifelse((y_high-y_low)<0, rad2deg(atan((y_high-y_low)/(x_high-x_low)))-ifelse((x_high-x_low)<0, 180, 0),rad2deg(atan((y_high-y_low)/(x_high-x_low)))+ifelse((x_high-x_low)<0, 180, 0)),2)) %>%
+  mutate(Diff_α=round(abs(Angle-mean_α),2))
 
 write.csv(clean_comets, file = "clean_comets.csv")
 
-clean_comets
+result1 <- clean_comets %>% filter(Diff_α>10) %>% nrow()
+result2 <- clean_comets %>% filter(Diff_α>10)
+result4 <- "Everything is OK"
+
+if(result1 > 0) cat("\n\nCheck the following track(s): ", result2$Track.ID) else(result4)
+
 
